a2e82c05c01df07696bf51ac1d594c72c59d22c9
maplepold <73566941+maplepold@users.noreply.github.com>
2022-05-13 13:01:54 +0700

    Update Flight_Info.java

--- src/System/Flight_Info.java
+++ src/System/Flight_Info.java
@@ -51,11 +51,11 @@ public class Flight_Info extends JFrame{  //Second Frame
         btnShow.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
 	
-                String code = textField.getText();
+                String code = textField.getText();           
                 
                 try {
                     Connector c = new Connector();
-                    String str = "select f_code,f_name,src,dst,capacity,class_code,class_name from flight ,airplane where f_code = '"+code+"'";
+                    String str = "select f_code,src,dst,capacity,class_name from flight ,airplane_sector where f_code = '"+code+"'and flight.f_name = airplane_sector.f_name" ;
       
                     ResultSet rs = c.s.executeQuery(str);
                     table.setModel(DbUtils.resultSetToTableModel(rs));	
@@ -71,10 +71,10 @@ public class Flight_Info extends JFrame{  //Second Frame
 		
         table = new JTable();
         table.setBackground(Color.WHITE);
-	table.setBounds(23, 250, 800, 300);
+	table.setBounds(23, 230, 800, 90);
         
         JScrollPane pane = new JScrollPane(table);
-	pane.setBounds(23, 250, 800, 300);
+	pane.setBounds(23, 240, 800, 90);
         pane.setBackground(Color.WHITE);
         add(pane);
 		
@@ -84,41 +84,29 @@ public class Flight_Info extends JFrame{  //Second Frame
 		
 	JLabel FlightCode = new JLabel("FLIGHT CODE");
 	FlightCode.setFont(new Font("Montserrat", Font.PLAIN, 13));
-        FlightCode.setBounds(55, 220, 126, 20);
+        FlightCode.setBounds(65, 220, 126, 20);
 	add(FlightCode);
 		
-	JLabel FlightName = new JLabel("FLIGHT NAME");
-        FlightName.setFont(new Font("Montserrat", Font.PLAIN, 13));
-	FlightName.setBounds(175, 220, 120, 20);
-	add(FlightName);
+	JLabel Source = new JLabel("FROM");
+	Source.setFont(new Font("Montserrat", Font.PLAIN, 13));
+	Source.setBounds(245, 220, 104, 20);
+	add(Source);
 		
-	JLabel From = new JLabel("FROM");
-	From.setFont(new Font("Montserrat", Font.PLAIN, 13));
-	From.setBounds(305, 220, 104, 20);
-	add(From);
-		
-	JLabel To = new JLabel("TO");
-	To.setFont(new Font("Montserrat", Font.PLAIN, 13));
-	To.setBounds(395, 220, 120, 20);
-	add(To);
-		
+	JLabel Destination = new JLabel("TO");
+	Destination.setFont(new Font("Montserrat", Font.PLAIN, 13));
+	Destination.setBounds(410, 220, 104, 20);
+	add(Destination);
 		
 	JLabel Capacity = new JLabel("CAPACITY");
 	Capacity.setFont(new Font("Montserrat", Font.PLAIN, 13));
-	Capacity.setBounds(460, 220, 111, 20);
+	Capacity.setBounds(545, 220, 111, 20);
 	add(Capacity);
-		
-	JLabel ClassCode = new JLabel("CLASS CODE");
-	ClassCode.setFont(new Font("Montserrat", Font.PLAIN, 13));
-	ClassCode.setBounds(565, 220, 120, 20);
-	add(ClassCode);
-		
-	JLabel ClassName = new JLabel("CLASS NAME");
+        
+        JLabel ClassName = new JLabel("CLASS NAME");
 	ClassName.setFont(new Font("Montserrat", Font.PLAIN, 13));
-	ClassName.setBounds(690, 220, 111, 20);
+	ClassName.setBounds(700, 220, 111, 20);
 	add(ClassName);
-		
-	
+			
         setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 	setSize(900,650);
         setVisible(true);

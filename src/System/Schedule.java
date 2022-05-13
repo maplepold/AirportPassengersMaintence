afc9f8b87ce2ad499d6f86dbf243fc051cf10537
maplepold <73566941+maplepold@users.noreply.github.com>
2022-05-13 13:00:30 +0700

    Update and rename Journey_Details.java to Schedule.java

--- src/System/Schedule.java
+++ src/System/Schedule.java
@@ -14,17 +14,17 @@ import java.sql.*;
 import javax.swing.*;
 import net.proteanit.sql.DbUtils;
 
-public class Journey_Details extends JFrame{  //Forth
+public class Schedule extends JFrame{  //Forth
 
     JTable table;
-    JLabel ReservationDetails,Pnrno,Ticketid,Fcode,Jnydate,Jnytime,From,To,label,label1;
+    JLabel ReservationDetails,Fcode,Fname,Jnydate,Jnytime,From,To,label,label1;
     JButton Show;
 
     public static void main(String[] args){
-        new Journey_Details();
+        new Schedule();
     }
     
-    public Journey_Details(){
+    public Schedule(){
         
         setTitle("JOURNEY DETAILS");
 	getContentPane().setBackground(Color.WHITE);
@@ -49,61 +49,61 @@ public class Journey_Details extends JFrame{  //Forth
 	setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		
 	ReservationDetails = new JLabel("JOURNEY DETAILS");
-	ReservationDetails.setForeground(Color.BLUE);
+	ReservationDetails.setForeground(Color.BLACK);
 	ReservationDetails.setFont(new Font("Montserrat", Font.PLAIN, 31));
 	ReservationDetails.setBounds(280, 27, 359, 31);
 	add(ReservationDetails);
-		
-	Pnrno = new JLabel("PNR CODE");
-	Pnrno.setFont(new Font("Montserrat", Font.PLAIN, 13));
-	Pnrno.setBounds(50, 185, 90, 20);
-	add(Pnrno);
-		
-	Ticketid = new JLabel("TICKET NO");
-	Ticketid.setFont(new Font("Montserrat", Font.PLAIN, 13));
-	Ticketid.setBounds(160, 185, 90, 20);
-	add(Ticketid);
-		
-	Fcode = new JLabel("FLIGHT NO");
+						
+	Fcode = new JLabel("FLIGHT CODE");
 	Fcode.setFont(new Font("Montserrat", Font.PLAIN, 13));
-	Fcode.setBounds(265, 185, 110, 20);
+	Fcode.setBounds(43, 185, 110, 20);
 	add(Fcode);
 		
-	Jnydate = new JLabel("DEPARTURE DATE");
+        Fname = new JLabel("FLIGHT NAME");
+	Fname.setFont(new Font("Montserrat", Font.PLAIN, 13));
+	Fname.setBounds(170, 185, 150, 20);
+	add(Fname);
+        
+	Jnydate = new JLabel("DATE");
 	Jnydate.setFont(new Font("Montserrat", Font.PLAIN, 13));
-	Jnydate.setBounds(370, 185, 130, 20);
+	Jnydate.setBounds(300, 185, 104, 20);
 	add(Jnydate);
 		
-	Jnytime = new JLabel("JOURNEY TIME");
+	Jnytime = new JLabel("TIME");
 	Jnytime.setFont(new Font("Montserrat", Font.PLAIN, 13));
-	Jnytime.setBounds(525, 185, 130, 20);
+	Jnytime.setBounds(429, 185, 130, 20);
 	add(Jnytime);
 		
 	From = new JLabel("FROM");
 	From.setFont(new Font("Montserrat", Font.PLAIN, 13));
-	From.setBounds(665, 185, 80, 20);
+	From.setBounds(558, 185, 80, 20);
 	add(From);
 		
 	To = new JLabel("TO");
 	To.setFont(new Font("Montserrat", Font.PLAIN, 13));
-	To.setBounds(750, 185, 80, 20);
+	To.setBounds(683, 185, 80, 20);
 	add(To);
      
         
-	String[] items1 =  {"HANOI","DA NANG","PHU QUOC","NHA TRANG","BUON MA THUOT","CA MAU","CAN THO","CHU LAI","CON DAO","DALAT","DIEN BIEN","DONG HOI","HAI PHONG","HUE","PLEIKU","QUY NHON","RACH GIA","THANH HOA","TUY HOA","VAN DON","VINH"};
+	String[] items1 =  {"HO CHI MINH","HA NOI","DA NANG","PHU QUOC","NHA TRANG","BUON MA THUOT","CA MAU","CAN THO","CHU LAI","CON DAO","DALAT","DIEN BIEN","DONG HOI","HAI PHONG","HUE","PLEIKU","QUY NHON","RACH GIA","THANH HOA","TUY HOA","VAN DON","VINH"};
 	JComboBox comboBox = new JComboBox(items1);
 	comboBox.setBounds(150, 100, 150, 27);
 	add(comboBox);
 		
 		
-	String[] items2 =  {"HANOI","DA NANG","PHU QUOC","NHA TRANG","BUON MA THUOT","CA MAU","CAN THO","CHU LAI","CON DAO","DALAT","DIEN BIEN","DONG HOI","HAI PHONG","HUE","PLEIKU","QUY NHON","RACH GIA","THANH HOA","TUY HOA","VAN DON","VINH"};
+	String[] items2 =  {"HO CHI MINH","HA NOI","DA NANG","PHU QUOC","NHA TRANG","BUON MA THUOT","CA MAU","CAN THO","CHU LAI","CON DAO","DALAT","DIEN BIEN","DONG HOI","HAI PHONG","HUE","PLEIKU","QUY NHON","RACH GIA","THANH HOA","TUY HOA","VAN DON","VINH"};
 	JComboBox comboBox_1 = new JComboBox(items2);
 	comboBox_1.setBounds(500, 100, 150, 27);
 	add(comboBox_1);
         
         table = new JTable();
-	table.setBounds(38, 310, 770, 130);
-	add(table);
+        table.setBackground(Color.WHITE);
+	table.setBounds(38, 210, 770, 190);
+        
+        JScrollPane pane = new JScrollPane(table);
+	pane.setBounds(38, 210, 790, 190);
+        pane.setBackground(Color.WHITE);
+        add(pane);
 	
 		
 		
@@ -114,14 +114,15 @@ public class Journey_Details extends JFrame{  //Forth
                     String dst  = (String) comboBox_1.getSelectedItem();
                     
                     Connector c = new Connector();
+               
+                    String str = "select f_code,f_name,jny_date,jny_time,src,dst from flight where src = '"+src+"' and dst = '"+dst+"'";
                     
-                    String str = "select pnr_no,ticket_id,f_code,jny_date,jny_time,src,dst from reservation where src = '"+src+"' and dst = '"+dst+"'";
-                    ResultSet rs=c.s.executeQuery(str);
+                    ResultSet rs= c.s.executeQuery(str);
                     
                     if(rs.next()){
                         table.setModel(DbUtils.resultSetToTableModel(rs));
                     }else{
-                        JOptionPane.showMessageDialog(null,"No Flights from FROM to TO ");
+                        JOptionPane.showMessageDialog(null,"No Flights from Source to Destination ");
                         
                     }					
                     
